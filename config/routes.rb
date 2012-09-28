eb644581bb029b75017e3983fd72c64488a4c3fe
Lydian <lydian@cmu.edu>
2012-09-28 03:27:09 -0700

    change root controller to students

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Service::Application.routes.draw do
   
   resources :attendence
   
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -62,7 +61,7 @@ Service::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'students#index'
 
   # See how all your routes lay out with "rake routes"
 
